3b9ea9f6507f02c0c44f11748172af3c43be5e2e
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2022-11-25 16:46:41 +0900

    vg

--- menu/script.js
+++ menu/script.js
@@ -3,13 +3,13 @@ const nav = document.querySelector('#js-nav');
 const box = document.querySelector('body');
 
 button.addEventListener('click', function () {
-    button.classList.toggle('active');
-    nav.classList.toggle('active');
-    box.classList.toggle('open');
+  button.classList.toggle('active');
+  nav.classList.toggle('active');
+  box.classList.toggle('open');
 });
 
 const menuCSS = document.createElement( "link" );
-menuCSS.href = "/menu/index.css";
+menuCSS.href = "/menu/style.css";
 menuCSS.type = "text/css";
 menuCSS.rel = "stylesheet";
 
